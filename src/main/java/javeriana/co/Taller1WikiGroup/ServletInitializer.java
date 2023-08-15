5a9f9c8dcc4856567a80d5c4541cfa2de86e4de7
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2023-08-14 21:22:28 -0500

    plantilla

---
+++ src/main/java/javeriana/co/Taller1WikiGroup/ServletInitializer.java
@@ -0,0 +1,13 @@
+package javeriana.co.Taller1WikiGroup;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Taller1WikiGroupApplication.class);
+	}
+
+}
---
+++ src/main/java/javeriana/co/Taller1WikiGroup/Taller1WikiGroupApplication.java
@@ -0,0 +1,13 @@
+package javeriana.co.Taller1WikiGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1WikiGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller1WikiGroupApplication.class, args);
+	}
+
+}
